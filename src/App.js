c4dbdd4ec2b1a588cf086f2ada34075de43e2415
meddil <med.maaoui.007@gmail.com>
2024-04-30 12:10:00 -0400

    commit 6

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from './Components/Navbar/Navbar';
+import Layout from './components/Landing_Page/Landing_page';
+
 function App() {
 
   return (
@@ -11,6 +13,7 @@ function App() {
           <Navbar/>
               <Routes>
               </Routes>
+              <Route path="/" element={<Landing_Page/>}/>
             
         </BrowserRouter>
        
